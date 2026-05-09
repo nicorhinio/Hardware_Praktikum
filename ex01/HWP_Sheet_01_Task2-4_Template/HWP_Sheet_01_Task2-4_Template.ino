3d61965c698586d7606e074c03b9e36a6e9e67b7
Daniel Götz <dangtz00@gmail.com>
2026-05-09 22:36:58 +0200

    Task 2 und 3

--- ex01/HWP_Sheet_01_Task2-4_Template/HWP_Sheet_01_Task2-4_Template.ino
+++ ex01/HWP_Sheet_01_Task2-4_Template/HWP_Sheet_01_Task2-4_Template.ino
@@ -5,20 +5,30 @@
 
 // Task 2 ii.) Constants — fill in the correct values from the datasheet
 //             You can also write the answers to the questions here.
+//             1.) 0x58 is the Address for SGP30
+//             2.) 0x2003
+//             3.) 0x2008
+//             4.) A Measurement Response is 6 Bytes long. 
+//                 It consists out of (One byte per element): CO2eq MSB Data, CO2eq LSB Data, CRC Checksum for CO2eq, TVOC MSB Data, TVOC LSB Data, CRC Checksum for TVOC
 // ------------------------------------------------------------
 
-#define SGP30_ADDR       0x00   // 7-bit I2C address of the SGP30 
+#define SGP30_ADDR       0x58   // 7-bit I2C address of the SGP30 
 
 // Command codes (2 bytes each, MSB first — see datasheet )
-#define CMD_INIT_MSB     0x00   //    first byte
-#define CMD_INIT_LSB     0x00   //    second byte
-#define CMD_MEAS_MSB     0x00
-#define CMD_MEAS_LSB     0x00
+#define CMD_INIT_MSB     0x20   //    first byte
+#define CMD_INIT_LSB     0x03   //    second byte
+#define CMD_MEAS_MSB     0x20
+#define CMD_MEAS_LSB     0x08
 
 // Display: air quality range for mapping CO2 to a percentage, you can change these to test more ranges
 #define CO2_MIN          400    // ppm — clean outdoor air
 #define CO2_MAX          2000   // ppm — poor indoor air quality
 
+uint16_t processed_co2 = 0;
+uint16_t processed_tvoc = 0;
+
+unsigned long last_measurement = 0;
+unsigned long current = 0;
 // ------------------------------------------------------------
 //  Display constructor
 // ------------------------------------------------------------
@@ -54,6 +64,10 @@ void sgp30_cmd(uint8_t msb, uint8_t lsb) {
   // TODO: open a transmission to SGP30_ADDR,
   //       write msb, write lsb, close the transmission.
   //       
+  Wire.beginTransmission(SGP30_ADDR);
+  Wire.write(msb);
+  Wire.write(lsb);
+  Wire.endTransmission();
 }
 
 
@@ -74,8 +88,16 @@ bool sgp30_read(uint8_t n) {
   //       Read the n bytes in order, remember wire.read() can only
   //       read one byte at a time. Look out for CRC bytes,  
   //       we don't need to store those.  
-  //      
-  return false;
+  if(Wire.requestFrom(SGP30_ADDR, n) != n){
+    return false;
+  }
+  raw_co2_msb = Wire.read();
+  raw_co2_lsb = Wire.read();
+  Wire.read();
+  raw_tvoc_msb = Wire.read();
+  raw_tvoc_lsb = Wire.read();
+  Wire.read();
+  return true;
 }
 
 
@@ -110,6 +132,17 @@ void display_values(uint16_t co2, uint16_t tvoc) {
   //
   // ------------------------------------------------------------
   // TODO (Task 3): set cursor, print co2 and tvoc values.
+  u8g2.clearBuffer();
+
+  u8g2.setCursor(0, 15);
+  u8g2.print("eCO2: ");
+  u8g2.print(co2);
+  u8g2.print("ppm   TVOC: ");
+  u8g2.print(tvoc);
+  u8g2.print("ppb");
+
+  u8g2.sendBuffer();
+
   // TODO (Task 4): map co2 to pct (0-100), draw a filled bar with
   //                u8g2.drawBox(x, y, width, height).
   //                Bar width  = map(pct, 0, 100, 0, 128)
@@ -125,33 +158,76 @@ void setup() {
   // TODO: Transmit to each available I2C address, print the adress when receiving an ACK.
   //       You can use decimal adresses when sending but convert them to hex when printing them out.
   //       Use Serial.print(address, HEX) to make it easier.
-
+  Wire.begin();
+  uint8_t addr = 8;
+  for(addr; addr < 128; addr++){
+    Wire.beginTransmission(addr);
+    if(Wire.endTransmission() == 0){
+      Serial.print("Found Device: ");
+      Serial.println(addr, HEX);
+    }
+  }
+  // Found addresses are: 3C; 51; 58
+  
   //     Task 2 iv.): Initialise SGP30 
   // TODO: send the init command, wait for initialization
   //       and print out a message.
-
+  sgp30_cmd(CMD_INIT_MSB, CMD_INIT_LSB);
+  delay(15000);
+  Serial.println("SPG30 initialized.");
  
 
   // --- Task 3 i.): Simple display use ---
   // TODO: initialize display, set a font, display "Hardware Praktikum 2026",
   //       and push it to the screen.
+  u8g2.begin();
+
+  u8g2.setFont(u8g2_font_helvB08_tf);
+
+  u8g2.clearBuffer();
+
+  u8g2.drawStr(0, 20, "Hardware");
+  u8g2.drawStr(0, 40, "Praktikum 2026");
+
+  u8g2.sendBuffer();
 }
 
 
 void loop() {
   // --- Task 2 iv.): Send measure command and read response ---
   // TODO: call sgp30_cmd() with the measure command bytes.
+  if (millis() - last_measurement >= 1000){
+  last_measurement = millis();
+  sgp30_cmd(CMD_MEAS_MSB, CMD_MEAS_LSB);
+  current = millis();
+  while (millis() < current + 12){
+    // timing measurement 12 ms
+  }
   // TODO: call sgp30_read().
   //       If it returns false, print an error message and return early.
+  if (sgp30_read(3) == false){
+    Serial.println("Error 404.");
+    return;
+  }
+  
   // TODO: Reconstruct 16-bit values from the raw bytes ----
+  processed_co2 = to_uint16(raw_co2_msb, raw_co2_lsb);
+  processed_tvoc = to_uint16(raw_tvoc_msb, raw_tvoc_lsb);
+
   // TODO: print co2 and tvoc with appropriate labels and units.
+  Serial.print("eCO2: ");
+  Serial.print(processed_co2);
+  Serial.print("ppm  ");
+  Serial.print("TVOC: ");
+  Serial.print(processed_tvoc);
+  Serial.println("ppb");
 
   // --- Task 3 ii.): Print the sgp30 values on the display
   //                  in addition to the Serial monitor 
-
+  display_values(processed_co2, processed_tvoc);
   // --- Task 4: Map CO2 to a percentage ---
   // TODO: use map() to scale co2 from raw values to 0-100%.
   //       Then use constrain() to make sure the percetange 
   //       doesnt go outside 0-100.
-
+  }
 }
