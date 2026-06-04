8cf2bf179fd5ef62fc58b305b071e1f6cbabebcd
Daniel Götz <dangtz00@gmail.com>
2026-06-04 21:51:50 +0200

    task6

--- ex02/exercise_templates/task6/task6.ino
+++ ex02/exercise_templates/task6/task6.ino
@@ -35,6 +35,7 @@ void loop() {
 void playMelody() {
   setBuzzerFreq(notes[0]);
   setTimer2(true);
+  setTimer1Freq();
   Serial.println("playMelody aufgerufen, Timer gestartet.");
   
 }
@@ -42,15 +43,16 @@ void playMelody() {
 void setBuzzerFreq(uint32_t newFreq) {
   freq = newFreq;
   if ((newFreq > 3000) || (newFreq < 100)){
-    NRF_TIMER2->TASKS_STOP = 1;
+    NRF_TIMER1->TASKS_STOP = 1;
     NRF_P0->OUTCLR = (1UL << BUZZER_PIN);
     return;
   }
   uint32_t compareValue = 1000000UL / (2 * freq);
-  NRF_TIMER2->TASKS_STOP = 1;
-  NRF_TIMER2->CC[0] = compareValue;
-  NRF_TIMER2->TASKS_CLEAR = 1;
-  NRF_TIMER2->TASKS_START = 1;
+  NRF_TIMER1->TASKS_STOP = 1;
+  NRF_TIMER1->CC[0] = compareValue;
+  NVIC_EnableIRQ(TIMER1_IRQn);
+  NRF_TIMER1->TASKS_CLEAR = 1;
+  NRF_TIMER1->TASKS_START = 1;
 }
 
 extern "C" void TIMER2_IRQHandler() {
@@ -65,6 +67,7 @@ extern "C" void TIMER2_IRQHandler() {
       Serial.println(melodyIdx);
       if (melodyIdx >= 10){
         setTimer2(false);
+        setBuzzerFreq(0);
       } else {
         setBuzzerFreq(notes[melodyIdx]);
       }
@@ -72,6 +75,19 @@ extern "C" void TIMER2_IRQHandler() {
   }
 }
 
+extern "C" void TIMER1_IRQHandler() {
+  if (NRF_TIMER1->EVENTS_COMPARE[0]) // check if compare0 was activated
+    {
+        NRF_TIMER1->EVENTS_COMPARE[0] = 0; // reset Event
+
+        buzzerState = !buzzerState; // switch buzzerstate
+
+        if (buzzerState)
+            NRF_P0->OUTSET = (1UL << BUZZER_PIN);
+        else
+            NRF_P0->OUTCLR = (1UL << BUZZER_PIN);
+    }
+}
 
 void setTimer2(bool enable) {
   NRF_TIMER2->TASKS_STOP = 1;
@@ -87,4 +103,23 @@ void setTimer2(bool enable) {
   NVIC_EnableIRQ(TIMER2_IRQn);
   NRF_TIMER2->TASKS_CLEAR = 1;
   NRF_TIMER2->TASKS_START = 1;
+}
+
+void setTimer1Freq() {
+  NRF_TIMER1->TASKS_STOP = 1; // stop Timer
+  NRF_TIMER1->MODE = TIMER_MODE_MODE_Timer; // select normal mode of timer
+
+  NRF_TIMER1->BITMODE = TIMER_BITMODE_BITMODE_32Bit; // Timer from 0 - 2^32 instead of 0 - 2^16
+  NRF_TIMER1->PRESCALER = 4;     // adapt tickrate -> 16 MHz / 2^PRESCALER = 1MHz
+
+  uint32_t compareValue = 1000000UL / (2 * freq); // set Compare Value
+
+  NRF_TIMER1->CC[0] = compareValue; // Compare Value into Compare Register (CC)
+  NRF_TIMER1->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk; // Stop Timer and Clear Timer, for compare with index 0 in CC
+  
+  NRF_TIMER1->INTENSET = TIMER_INTENSET_COMPARE0_Msk; // activate Interrupt if Compare0 is reached
+  NVIC_EnableIRQ(TIMER1_IRQn); // react to interrupt
+
+  NRF_TIMER1->TASKS_CLEAR = 1; // reset Timer -> activate clear -> Timer = 0
+  NRF_TIMER1->TASKS_START = 1; // start Timer
 }
