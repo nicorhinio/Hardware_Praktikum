04e6fdf03895c92908774e8fcd8f8c2650c1a253
Daniel Götz <dangtz00@gmail.com>
2026-06-17 18:12:58 +0200

    task 3 and 4

--- ex03/Ex3.3-template/Ex3.3-template.ino
+++ ex03/Ex3.3-template/Ex3.3-template.ino
@@ -5,27 +5,53 @@
 
 // --- Objects ---
 Adafruit_SGP30 sgp;
-
+unsigned long lastMeasurement = 0;
 // --- Timing ---
 // TODO: define sampling interval (1 second)
-
+const unsigned long interval = 1000;
 // --- Warm-up ---
 // TODO: define warm-up duration (15 seconds)
+const unsigned long warmupInterval = 15000;
+unsigned long warmupStartTime = 0;
 // TODO: maintain a warm-up state indicator
+bool warmupSensorReady = false;
 
 void setup() {
     Serial.begin(115200);
     while (!Serial && millis() < 3000);
 
     // TODO: initialize the sensor and handle initialization failure
-
+    sgp.begin();
     // TODO: start air quality measurements
-
+    warmupStartTime = millis();
     // TODO: record system start time
 }
 
 void loop() {
     unsigned long now = millis();
+
+    if((millis() - lastMeasurement) >= interval){
+        if (!sgp.IAQmeasure()) {
+            Serial.println("SGP30 measurement failed!");
+            return;
+        }
+        lastMeasurement = millis();
+        if (!warmupSensorReady){
+            Serial.print("UNSTABLE/WARMUP ");
+        }
+        Serial.print("eCO2: ");
+        Serial.print(sgp.eCO2);
+        Serial.println("ppm");
+        Serial.print("TVOC: ");
+        Serial.print(sgp.TVOC);
+        Serial.println("ppb");
+        if ((now - warmupStartTime) >= warmupInterval && !warmupSensorReady){
+            warmupSensorReady = true;
+            Serial.println("Warm-Up finished.");
+        }else if (!warmupSensorReady){
+            Serial.println("Still in Warm-Up...");
+        }
+    }
 
     // TODO: implement periodic sampling at 1 Hz using millis()
 
--- ex03/Ex3.4-template/Ex3.4-template.ino
+++ ex03/Ex3.4-template/Ex3.4-template.ino
@@ -6,8 +6,11 @@
 #include <U8g2lib.h>
 #include <DHT.h>
 
+
 // --- Hardware Configuration ---
 // TODO: define pins (DHT, light sensor, LED, buzzer)
+
+#define SENSOR_TYPE DHT11
 
 // --- System Constants ---
 // TODO: define timing constants for:
@@ -16,6 +19,22 @@
 // - BLE transmission (1 Hz)
 // - warm-up duration (30 s)
 
+uint16_t rawValue = 0;
+int16_t adc_result;
+unsigned long lightLastMeasurement = 0;
+const unsigned long lightInterval = 500;
+
+
+int TEMP_PIN = 7;
+DHT dht_sensor(TEMP_PIN, SENSOR_TYPE);
+unsigned long lastSample = 0;
+unsigned long dhtLastMeasurement = 0;
+const unsigned long dhtInterval = 2000;
+float lastTemp = 0;
+float lastHum = 0;
+int failureCounter = 0;
+const float a = 17.62;
+const float b = 243.12;
 // --- Objects ---
 // TODO: initialize display, sensors, BLE service and characteristic
 
@@ -40,7 +59,22 @@ SystemState currentState = STATE_INIT;
 void setup() {
     Serial.begin(115200);
 
+    while(!Serial){
+        delay(10);
+    }
     // TODO: initialize hardware (pins, display, sensors)
+    Serial.println("Starting...");
+
+    NRF_SAADC->ENABLE = SAADC_ENABLE_ENABLE_Enabled;
+    NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_12bit;
+    NRF_SAADC->CH[0].PSELP = SAADC_CH_PSELP_PSELP_AnalogInput0;
+    NRF_SAADC->CH[0].CONFIG = (SAADC_CH_CONFIG_GAIN_Gain1_6 << SAADC_CH_CONFIG_GAIN_Pos) | (SAADC_CH_CONFIG_REFSEL_Internal << SAADC_CH_CONFIG_REFSEL_Pos) | (SAADC_CH_CONFIG_TACQ_10us << SAADC_CH_CONFIG_TACQ_Pos);
+    NRF_SAADC->RESULT.PTR = (uint32_t)&adc_result;
+    NRF_SAADC->RESULT.MAXCNT = 1;
+    Serial.println("SAADC configurated.");
+
+    dht_sensor.begin();
+    delay(2000);
 
     // TODO: initialize BLE and start advertising
 
@@ -54,6 +88,53 @@ void loop() {
 
     // i) TODO: asynchronous sensor acquisition (light, DHT, SGP30)
 
+    //Light Sensor Measurement.
+    if (now - lightLastMeasurement >= lightInterval){
+        lightLastMeasurement = now;
+    
+        rawValue = saadcRawRead();
+        Serial.print("raw value: ");
+        Serial.println(rawValue);
+
+        if (rawValue < 50){
+            rawValue = 50;
+        }else if(rawValue > 3500){
+            rawValue = 3500;
+        }
+        long mapping = map(rawValue, 50, 3500, 0, 100);
+        mapping = constrain(mapping, 0, 100);
+
+    }
+    // Temp and Humidity Measurement.
+    if (now - dhtLastMeasurement >= dhtInterval){
+        dhtLastMeasurement = now;
+
+        float humidity = dht_sensor.readHumidity();
+        float temperatureCelsius = dht_sensor.readTemperature();
+
+    if (isnan(temperatureCelsius) || isnan(humidity)){
+        failureCounter++;
+        if(isnan(temperatureCelsius)){
+            temperatureCelsius = lastTemp;
+        } 
+        if(isnan(humidity)){
+            humidity = lastHum;
+        }
+    } else{
+        lastTemp = temperatureCelsius;
+        lastHum = humidity;
+        failureCounter = 0;
+    }
+
+    if (failureCounter >= 5){
+        Serial.print("[WARNING] DHT failures: ");
+        Serial.println(failureCounter);
+    }
+    float currentDewPoint = computeDewPoint(temperatureCelsius, humidity);
+
+    // Air Quality Measurement.
+
+
     // iii) TODO: warm-up handling (STATE_INIT for 30 s)
 
     // iii) TODO: compute health score (0–100)
@@ -65,4 +146,31 @@ void loop() {
     // v) TODO: implement LED and buzzer behavior (non-blocking)
 
     // vi) TODO: send BLE telemetry (formatted string, 1 Hz)
+}
+
+uint16_t saadcRawRead(){
+    NRF_SAADC->EVENTS_END = 0;
+    NRF_SAADC->TASKS_START = 1;
+    while(!NRF_SAADC->EVENTS_STARTED);
+    NRF_SAADC->EVENTS_STARTED = 0;
+    NRF_SAADC->TASKS_SAMPLE = 1;
+    while(!NRF_SAADC->EVENTS_END);
+    NRF_SAADC->TASKS_STOP = 1;
+    return adc_result;
+}
+
+const char* categorize(int normalized) {
+    if (normalized < 30){
+        return "LOW";
+    }else if(normalized < 70){
+        return "MEDIUM";
+    }else{
+        return "HIGH";
+    }
+}
+
+float computeDewPoint(float tempC, float relHum) {
+    float gamma = ((a * tempC) / (b + tempC)) + log(relHum / 100);
+    float dewPoint = (b * gamma) / (a - gamma);
+    return dewPoint;
 }
